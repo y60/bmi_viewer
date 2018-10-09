eea2a34c8659e7b68f0c5dcc61336a153ffa3425
Yusuke Hara <yh@g.ecc.u-tokyo.ac.jp>
2018-10-09 16:16:50 +0900

    標高もスケールするようにする

--- scaler.py
+++ scaler.py
@@ -4,4 +4,5 @@ import os
 args = sys.argv
 arr = pd.read_csv(args[1],header=None).values
 w=int(args[2])
-pd.DataFrame(arr[1::w,1::w]).to_csv(os.path.splitext(args[1])[0]+"_"+args[2]+".csv",header=None,index=None)
+arr=arr[1::w,1::w]/w
+pd.DataFrame(arr).to_csv(os.path.splitext(args[1])[0]+"_"+args[2]+".csv",header=None,index=None)
