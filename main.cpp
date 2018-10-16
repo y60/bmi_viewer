f84783dd8852b086e5cf9a6775808b4848df5b7f
Yusuke Hara <yh@g.ecc.u-tokyo.ac.jp>
2018-10-16 15:34:31 +0900

    地名データの表示

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ using namespace std;
 
 int load_csv(char* file);
 void load_gsx_csv(char* file);
+void load_poll_csv(char* file);
 void init_GL(int argc, char *argv[]);
 void set_callback_functions();
 void draw_map();
@@ -24,13 +25,14 @@ void glut_motion(int x,int y);
 void draw_sky();
 void draw_gsx();
 void draw_polls();
-void draw_poll(float* x,float* color);
+void draw_poll(float* x,float* color,string str);
 void convertLatLon(float lat, float lon, float* xyz);
 GLfloat green_dif[] ={88.0/255,181.0/255,64/255,1.0};
 GLfloat green_amb[] ={30.0/255,33.0/255,19.0/255,1.0};
 GLfloat red_line[] ={1,0,0,1.0};
 
 int gsx_size;
+int poll_size;
 double lat;
 double lon;
 double g_angle1 = 0.0;
@@ -43,6 +45,8 @@ double g_angle2 = -3.141592 / 6;
 int w,h;
 GLfloat* gsx_data;
 GLfloat* csv_data;
+GLfloat* poll_data;
+string* poll_name_data;
 GLuint* indices;
 GLfloat* normals;
 GLfloat* colors;
@@ -53,6 +57,7 @@ int main(int argc, char* argv[]){
     w=stoi(argv[3]);
     int size = load_csv(argv[1]);
     load_gsx_csv(argv[4]);
+    load_poll_csv(argv[5]);
     pos_x=w/2;
     pos_y=h/2;
     printf("%d*%d map %s loaded.\n",w,h,argv[1]);
@@ -128,6 +133,31 @@ void load_gsx_csv(char* file){
     pos_z=gsx_data[2]+100;
     printf("gsx loaded.\n");
 }
+
+void load_poll_csv(char* file){
+    ifstream ifs(file);
+    string buf;
+    getline(ifs, buf);
+    poll_size=stoi(buf);
+    printf("poll count:%d\n",poll_size);
+    poll_data = (float *)malloc(sizeof(float)*poll_size*3);
+    poll_name_data = (string *)malloc(sizeof(string)*poll_size);
+    float *p=poll_data;
+    int count=0;
+    while (getline(ifs, buf,',')&&count<poll_size) {
+        float mlat=stof(buf);
+        getline(ifs, buf,',');
+        float mlon=stof(buf);
+        getline(ifs, buf,'\n');
+        printf("%s\n",buf);
+        buf.copy(poll_name_data[count],buf.size());
+        convertLatLon(mlat,mlon,p);
+        p+=3;
+        count++;
+    }
+    printf("poll loaded.\n");
+}
+
 void convertLatLon(float mlat, float mlon, float* xyz){
     xyz[0]=(mlon-lon)*9000;
     xyz[1]=-(mlat-lat)*9000;
@@ -306,22 +336,27 @@ void draw_gsx(){
     glBegin(GL_LINE_STRIP);
     float *p=gsx_data;
     for(int i=0;i<gsx_size;i++){
-        glVertex3d(p[0],p[1],p[2]+1.0);
+        glVertex3f(p[0],p[1],p[2]+1.0);
         p+=3;
     }
     glEnd();
 }
 
 void draw_polls(){
-    draw_poll(gsx_data,red_line);
-    draw_poll(gsx_data+(gsx_size-1)*3,red_line);
+    draw_poll(gsx_data,red_line,"Start");
+    draw_poll(gsx_data+(gsx_size-1)*3,red_line,"Goal");
+    for (int i = 0;i<poll_size;i++){
+      draw_poll(poll_data+i*3,red_line,poll_name_data[i]);
+    }
 }
-void draw_poll(float* x,float* color){
+void draw_poll(float* x,float* color,string str){
     glMaterialfv(GL_FRONT,GL_DIFFUSE,color);
     glBegin(GL_LINE_STRIP);
-    
-    glVertex3d(x[0],x[1],x[2]);
-    glVertex3d(x[0],x[1],pos_z+10);
-
+    glVertex3f(x[0],x[1],x[2]);
+    glVertex3f(x[0],x[1],pos_z+1);
     glEnd();
+    glRasterPos3f(x[0],x[1],pos_z);
+    for(int i = 0;i<str.size();i++){
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15,str[i]);
+    }
 } 
